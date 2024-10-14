99cb17a3437544a1fdbc849c7bc6fe2e0e33d17e
Chris-Graham-MSF <christopher.graham@newyork.msf.org>
2024-10-14 14:48:11 -0400

    Updates carousel text and constructs the basis of a set of instructions. Changes button reference text to 'Submission Details'

--- src/app/components/CarouselComponent.tsx
+++ src/app/components/CarouselComponent.tsx
@@ -6,19 +6,19 @@ function CarouselComponent() {
   const carouselItems = [
     {
       id: 1,
-      title: 'First Slide',
-      description: 'Welcome to the Science Portal Take home test! This first slide is meant to be an example displaying the capabilities of this very basic carousel. What you will notice is that this long chunk of text does not overflow beyond the bounds of the component, and that a user is able to scroll down through the text in order to see more. \n\n Long before the events of Elden Ring, the world was shrouded in fog and ruled by Everlasting Dragons. This era was known as the Age of the Archtrees, a time of gray crags, archtrees, and immortal dragons.\n\n The status quo changed dramatically with the arrival of the Greater Will, an outer god of immense power. The Greater Will sent a golden star crashing into the Lands Between, which would later be known as the Elden Beast. This cosmic entity became the embodiment of order in the world...',
+      title: 'What is the Science Portal?',
+      description: 'Welcome to the Science Portal Take home test! \n\n The MSF Science Portal is a digital platform for Médecins Sans Frontières to share the medical evidence we gather as part of our medical humanitarian work aiding people and communities affected by conflict, epidemics, disasters, or exclusion from health services. Here you will find the peer-reviewed journal articles, MSF/Epicentre Scientific Days conference materials and other expert content that emerges from the hundreds of research studies and analyses we do each year, all aimed at improving patient care and advocating for evidence-based policy and practices. The site also features curated collections of our work in specific medical topics and ongoing crises, providing good entry points into what is often a large amount of relevant content in a given area.',
       imageSrc: 'https://www.doctorswithoutborders.org/sites/default/files/styles/crop_article_desktop_1238_529/public/image_base_media/2019/05/MSF134504.jpg?itok=keKI0rga',
       linkUrl: '/instructions',
-      linkTitle: 'Learn More'
+      linkTitle: 'Submission Details'
     },
     {
       id: 2,
-      title: 'Second Slide',
-      description: 'Sample text for the second slide. This is another example of content.',
+      title: 'Instructions',
+      description: 'A successful submission will be one where the individual carousel slides have been broken out accordingly. \n\n The slides should be broken out in a manner that extracts the first slide into its own component and has it displayed prominently above the other slides. The other slides should be converted into a general card component, which when clicked reveals the associated text as dropdown text. These card components should be arranged in a grid layout. ',
       imageSrc: 'https://www.doctorswithoutborders.org/sites/default/files/styles/crop_article_mobile_335_189/public/image_base_media/2022/06/MSB127143.jpg?itok=nIKK3T18',
       linkUrl: '/instructions',
-      linkTitle: 'Learn More'
+      linkTitle: 'Submission Details'
     },
     {
         id: 3,
@@ -26,7 +26,7 @@ function CarouselComponent() {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n\n Nulla facilisi. Maecenas non lectus in lacus varius venenatis. Proin sagittis, nisi in tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc. Sed euismod, nunc vel tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc. Sed euismod, nunc vel tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc.',
         imageSrc: 'https://www.doctorswithoutborders.org/sites/default/files/MSF163911%28High%29_0.jpg',
         linkUrl: '/instructions',
-        linkTitle: 'Learn More'
+        linkTitle: 'Submission Details'
       },
       {
         id: 4,
@@ -34,7 +34,7 @@ function CarouselComponent() {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. \n\n Nulla facilisi. Maecenas non lectus in lacus varius venenatis. Proin sagittis, nisi in tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc. Sed euismod, nunc vel tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc. Sed euismod, nunc vel tincidunt tincidunt, nunc nulla aliquam nunc, vel tincidunt nisi nunc vel nunc. \n\n Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod.',
         imageSrc: 'https://www.doctorswithoutborders.org/sites/default/files/MSF152109.jpg',
         linkUrl: '/instructions',
-        linkTitle: 'Learn More'
+        linkTitle: 'Submission Details'
       }
   ];
 
